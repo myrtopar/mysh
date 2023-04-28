9e0d305b45fc6ed151d13150c1eda7d03022b88a
myrtopar <myrtopar08@gmail.com>
2023-04-28 12:39:27 +0300

    correct makefile

--- Makefile
+++ Makefile
@@ -2,7 +2,9 @@ mysh: mysh.c functions.c
 	gcc -c mysh.c
 	gcc -c functions.c
 	gcc -c wildcard.c
-	gcc -o mysh mysh.o functions.o wildcard.o
+	gcc -c history.c
+	gcc -c alias.c
+	gcc -o mysh mysh.o functions.o wildcard.o history.o alias.o
 
 clean:
 	rm -f mysh *.o
--- wildcard.c
+++ wildcard.c
@@ -14,10 +14,8 @@ void expand_asterisk(char **token_array, int in_redirect, int out_redirect, int 
         {
             if ((res = glob(token_array[i], 0, NULL, &files)) == 0)
             {
-                printf("token: %s,\n", token_array[i]);
                 for (size_t k = 0; k < files.gl_pathc; k++)
                 {
-                    printf("%s, %d\n", files.gl_pathv[k], j);
                     expanded_token_array[j++] = files.gl_pathv[k];
                 }
             }
@@ -31,9 +29,9 @@ void expand_asterisk(char **token_array, int in_redirect, int out_redirect, int 
     }
     expanded_token_array[j] = NULL;
 
-    // int m = 0;
-    // while (expanded_token_array[m] != NULL)
-    //     printf("%s\n", expanded_token_array[m++]);
+    int m = 0;
+    while (expanded_token_array[m] != NULL)
+        printf("%s\n", expanded_token_array[m++]);
 
     globfree(&files);
 }
